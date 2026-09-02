f57ac53c5736b6e8addb6a937a65ac684dadf97f
Your Name <you@example.com>
2026-09-02 20:57:31 +0530

    flight and tavily tools added

---
+++ test.py
@@ -0,0 +1,8 @@
+from tools.tavily_tool import tavily_search
+from tools.flight_tool import search_flights
+
+# res=tavily_search("best hotels in INDIA")
+
+res =search_flights("Plan a 7 days Japan trip from Bangladesh")
+
+print(res)
